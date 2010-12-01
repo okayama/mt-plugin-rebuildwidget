aab0026bdcec86c59d53b8630ed1440b74e182a0
okayama <kenmin.okayama@gmail.com>
2010-12-01 18:55:40 +0900

    First commit

---
+++ plugins/RebuildWidget/lib/MT/RebuildWidget/L10N/en_us.pm
@@ -0,0 +1,11 @@
+package MT::RebuildWidget::L10N::en_us;
+
+use strict;
+use base qw/ MT::RebuildWidget::L10N MT::L10N MT::Plugin::L10N /;
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    '_PLUGIN_DESCRIPTION' => 'Show links for rebuild blogs in widget in editting template of website.',
+);
+
+1;
---
+++ plugins/RebuildWidget/lib/MT/RebuildWidget/L10N/ja.pm
@@ -0,0 +1,11 @@
+package MT::RebuildWidget::L10N::ja;
+
+use strict;
+use base qw/ MT::RebuildWidget::L10N MT::L10N MT::Plugin::L10N /;
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    '_PLUGIN_DESCRIPTION' => 'ウェブサイトのテンプレート編集画面にブログ再構築のためのリンクウィジェットを表示します。',
+);
+
+1;
